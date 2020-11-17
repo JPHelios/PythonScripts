cccf8c95a1222315805af1e52962263a49d3fbd6
I538909 <johannes.peters@sap.com>
2020-11-17 13:48:56 +0100

    code structure; all four functions are working now

--- AmazonScrapper/URLs.py
+++ AmazonScrapper/URLs.py
@@ -1,31 +1,42 @@
 import pickle
 import os
+import msvcrt
 
-os.system("cls")
+##################################################################################
+#FUNCTIONS
+##################################################################################
 
-#Print already existing URLs
-urls = pickle.load(open("data.txt", "rb"))
-for i in range(0, len(urls)):
-    print("...............................")
-    print(urls[i])
+def print_overview():
 
-print("...............................")
-
-for i in range(0,3):
+    #Print an overview of all possible actions in the cli
+    
     print(" ")
+    print("What do you want to do?")
+    print("++++++++++++++++++++++++++++++++")
+    print("+                              +")
+    print("+  1: View URLs                +")
+    print("+  2: Add a new URL            +")
+    print("+  3: Delete an existing URL   +")
+    print("+  4: Quit                     +")
+    print("+                              +")
+    print("++++++++++++++++++++++++++++++++")
+    print(" ")  
 
-#Choose action
-print("What do you want to do?")
-print("++++++++++++++++++++++++++++++++")
-print("+                              +")
-print("+  1: View URLs                +")
-print("+  2: Add a new URL            +")
-print("+  3: Delete an existing URL   +")
-print("+                              +")
-print("++++++++++++++++++++++++++++++++")
-print(" ")
+    # options = ["View URLs", "Add a new URL", "Delete an existing URL"]
+    # divider = "++++++++++++++++++++++++++++++++"
+    # print("What do you want to do?")
+    # print(divider)
+    # for i in range(0, len(options)):
+    #     print(f"+  {i+1}: {options[i]}")
+    # print(divider)
+
+##################################################################################
 
 def choose():
+
+    #Choose function to handle the input from the user
+    #Endless Loop to run the input several times until it is stopped by an if-Statement
+    #Error Exception if invalid characters are inserted
 
     while True:
         action = input("Please enter one option: ")
@@ -37,11 +48,76 @@ def choose():
         except Exception: 
             action = 0
 
-        if(action == 1 or action == 2 or action == 3):
+        if(action > 0 and action < 5):
             break
 
     return action
 
+##################################################################################
 
-action = choose()
-print(f"You choose: {action}" )
+def display():
+
+    #Print the data from the data file in cli
+
+    for i in range(0, len(urls)):
+        print("...............................")
+        print(urls[i])
+
+    print("...............................")
+    handshake()
+
+##################################################################################
+
+def add_url(urls):
+
+    #Add an URL the data file
+    new_url = input("Please add the link here: ")
+    
+    urls.append(new_url)
+    print("...")
+    print("Entry successfully written")
+
+    pickle.dump(urls, open("data.txt", "wb"))
+
+    handshake()
+
+##################################################################################
+
+def delete_url(urls):
+
+    #Delete a URL from the data file
+    index = int(input("Please insert an index value: "))
+    urls.pop(index-1)
+
+    pickle.dump(urls, open("data.txt", "wb"))
+    print(" ")
+    print("Item successfully deleted")
+    handshake()
+
+##########################################################################################
+def handshake():
+    print(" ")
+    print("Press any key to continue...")
+    print(" ")
+    msvcrt.getch()
+    os.system("cls")
+
+##########################################################################################
+#PROGRAM CODE
+##########################################################################################
+while True:
+
+    urls = pickle.load(open("data.txt", "rb"))
+
+    print_overview() 
+    action = choose()
+
+    os.system("cls") 
+
+    if action == 1: display()
+    elif action == 2: add_url(urls)
+    elif action == 3: delete_url(urls)
+    elif action == 4: break
+
+
+    
