684561f0789fb617ee18a89468c9d9ab7cec67f2
I538909 <johannes.peters@sap.com>
2020-11-17 10:00:58 +0100

    Catch Exceptions and bugfix Loop in choose()

--- AmazonScrapper/URLs.py
+++ AmazonScrapper/URLs.py
@@ -1,49 +1,47 @@
 import pickle
 import os
 
-os.system("clr")
+os.system("cls")
 
 #Print already existing URLs
-urls = pickle.load(open("names.txt", "rb"))
+urls = pickle.load(open("data.txt", "rb"))
 for i in range(0, len(urls)):
     print("...............................")
     print(urls[i])
 
-for i in range(0,5):
+print("...............................")
+
+for i in range(0,3):
     print(" ")
 
 #Choose action
 print("What do you want to do?")
 print("++++++++++++++++++++++++++++++++")
 print("+                              +")
-print("+  1: Add a new URL            +")
-print("+  2: Delete an existing URL   +")
+print("+  1: View URLs                +")
+print("+  2: Add a new URL            +")
+print("+  3: Delete an existing URL   +")
 print("+                              +")
 print("++++++++++++++++++++++++++++++++")
 print(" ")
 
 def choose():
-    action = input("Please enter one option: ")
 
-
-    while((action != 1) and (action != 2)):
+    while True:
         action = input("Please enter one option: ")
-        print("wrong number")
+        
+        try:
+            action = int(action)
+        except NameError:
+            action = 0
+        except Exception: 
+            action = 0
 
-    return action()
-    # if(action == 1):
-    #     return action
-    # elif(action == 2):
-    #     return action
-    # else: 
-    #     choose()
+        if(action == 1 or action == 2 or action == 3):
+            break
 
-    
-
+    return action
 
 
 action = choose()
-
-
-
 print(f"You choose: {action}" )
