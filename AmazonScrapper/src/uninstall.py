ec6613891addcb98fef42c5dcb046ff884b6f762
I538909 <johannes.peters@sap.com>
2021-01-20 17:28:50 +0100

    Create uninstall.py

---
+++ AmazonScrapper/src/uninstall.py
@@ -0,0 +1,11 @@
+import os
+
+os.path.abspath(__file__)
+
+if os.path.isfile('c:/Users/I538909/Documents/GitHub/PythonScripts/AmazonScrapper/data.txt'): print('data.txt already exists ')  
+
+if os.path.isfile('data.txt'): print('data.txt already exists') 
+else: print('data.txt does not exist')
+
+#c:/Users/I538909/Documents/GitHub/PythonScripts/AmazonScrapper/src/uninstall.py
+#else: print('data.txt does not exist')
